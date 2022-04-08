eee6381a710054f9b2b531b53aa7ec217b1d6ca6
nishit47 <nishitthapa47@gmail.com>
2022-04-08 10:52:09 +0545

    first
    it stops at 41 because instead of scraping the rating,it instead scrapes the title attribute in the tag.
    //needs fixing

---
+++ topbooks.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+from csv import writer
+
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+url="https://www.goodreads.com/list/show/13086.Goodreads_Top_100_Literary_Novels_of_All_Time"
+page=requests.get(url)
+
+soup= BeautifulSoup(page.content, 'html.parser')
+lists=soup.find_all('tr')
+
+with open('goodreads_top100.csv', 'w', encoding='utf8', newline='') as f:
+    thewriter=writer(f)
+    heading=["Rank", "Name", "Author", "Rating"]
+    thewriter.writerow(heading)
+
+    for list in lists:
+        ranking=list.find('td', class_="number").text
+        title=list.find('a', class_="bookTitle").span.text
+        author=list.find('a', class_="authorName").span.text
+        ratingString=(list.find('span', class_="minirating").text[1:5])
+        rating=float(ratingString)
+        data=[ranking, title, author,rating]
+        print(data)
+        thewriter.writerow(data)
+
+df=pd.read_csv("goodreads_top100.csv")
+
+profile=ProfileReport(df)
+profile.to_file(output_file="GoodreadsReport.html")
